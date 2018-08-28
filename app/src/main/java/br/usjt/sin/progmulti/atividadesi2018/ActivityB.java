9f35503d4beb9ef464c7d96ef75c95bfe39b9a80
Marcela Menezes Pinto <FY0102@dc.com.br>
2018-08-28 20:55:46 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/sin/progmulti/atividadesi2018/ActivityB.java
@@ -0,0 +1,15 @@
+package br.usjt.sin.progmulti.atividadesi2018;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityB extends Activity {
+
+    /*Marcela Menezes - 816117695*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
